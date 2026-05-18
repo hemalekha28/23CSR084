cf69f68039e8e24c51f064aaebe0b636ce4b33b6
HEMALEKHA <155727502+hemalekha28@users.noreply.github.com>
2026-05-18 12:15:24 +0530

    Add files via upload

---
+++ TopNotications.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+export default function TopNotications()  {
+    return(
+        <>
+            app.fetch('http://4.224.186.213/evaluation-service/otifications').
+            then(console.log(res.notifications)).
+            err('API not found');
+        </>
+    )
+}
